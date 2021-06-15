8513b0bf41a96e22e08bf8f3bcdd6817459a744b
PonkeerthanaNatarajan <85806084+PonkeerthanaNatarajan@users.noreply.github.com>
2021-06-15 21:41:44 +0530

    Add files via upload

---
+++ Day-4 Exercise.py
@@ -0,0 +1,41 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Creating three variables:
+>>> a=20
+>>> b=7
+>>> c=-15
+>>> #a)Dividing a by 10
+>>> a/10
+2.0
+>>> #b)Multiplying b by 50
+>>> b*50
+350
+>>> #c)Adding c by 60
+>>> c+60
+45
+>>> #2)Creating string variable:
+>>> V="GINJI"
+>>> #a)Replacing 3rd character with G
+>>> V=V[:2]+"G"+V[3:]
+>>> print(V)
+GIGJI
+>>> #3)Two values (a,b) of int,float datatype
+>>> a=5
+>>> b=6.0
+>>> print(type(a))
+<class 'int'>
+>>> print(type(b))
+<class 'float'>
+>>> #a)Converting a from int to float datatype
+>>> a=float(a)
+>>> print(a)
+5.0
+>>> print(type(a))
+<class 'float'>
+>>> #b)Converting b from float to int datatype
+>>> b=int(b)
+>>> print(b)
+6
+>>> print(type(b))
+<class 'int'>
+>>> 
